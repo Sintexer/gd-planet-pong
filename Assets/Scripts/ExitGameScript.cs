97d21873c97b8a20e1965d7abebf6439c8076d34
Ilya Buglakov <ilboogl@gmail.com>
2025-12-02 00:22:20 +0300

    feat: final touches and fixes

--- Assets/Scripts/ExitGameScript.cs
+++ Assets/Scripts/ExitGameScript.cs
@@ -1,29 +1,40 @@
-    using System.Collections;
-    using UnityEngine;
+using System.Collections;
+using UnityEngine;
 
-    public class ExitGameScript : MonoBehaviour
+public class ExitGameScript : MonoBehaviour
+{
+    [SerializeField] private float exitDelay = 0.8f;   // how long the click SFX lasts
+    private bool isQuitting;
+
+    [SerializeField]
+    private GameObject browserExitTipPanel;
+
+    public void ExitGame()
     {
-        private bool exited;
-        
-        public void ExitGame()
-        {
-            if (!exited)
-            {
-                // StartCoroutine(ExitRoutine());
-                SFX.Instance.Play("click_exit");
-                // yield return new WaitForSeconds(1f); // play sound
-                Application.Quit();
-                Debug.Log("Exiting game...");
-                exited = true;
-            }
-        }
-
-        // TODO Does not work
-        private IEnumerator ExitRoutine()
-        {
-            SFX.Instance.Play("click_exit");
-            yield return new WaitForSeconds(1f); // play sound
-            Application.Quit();
-            Debug.Log("Exiting game...");
-        }
+        if (isQuitting) return;
+        StartCoroutine(ExitRoutine());
     }
+
+    private IEnumerator ExitRoutine()
+    {
+        isQuitting = true;
+
+        if (SFX.Instance) SFX.Instance.Play("click_exit");
+
+        // Wait in real time so it works even if timeScale = 0 (pause menu)
+        yield return new WaitForSecondsRealtime(exitDelay);
+
+        QuitPlatformSafe();
+    }
+
+    private void QuitPlatformSafe()
+    {
+        Debug.Log("Exiting game...");
+
+// #if UNITY_EDITOR
+//         UnityEditor.EditorApplication.isPlaying = false;
+// #endif
+        browserExitTipPanel.SetActive(true);
+        Application.Quit();
+    }
+}
